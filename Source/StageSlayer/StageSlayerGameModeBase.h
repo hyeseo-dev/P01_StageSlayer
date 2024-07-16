30f9ecae5c543e7a80094c3011ca90872ffb482f
hyeseo <hyeseo.dev@gmail.com>
2024-07-16 23:48:11 +0900

    Initialize

---
+++ Source/StageSlayer/StageSlayerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "StageSlayerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STAGESLAYER_API AStageSlayerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
